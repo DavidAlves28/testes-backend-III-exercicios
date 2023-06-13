db0850795897d15f108d19b7ef6c4d8c11604db9
david alves <dav.alvesc28@gmail.com>
2023-06-13 19:30:31 -0300

    resolucao business 100%

--- tests/business/product/getProducts.test.ts
+++ tests/business/product/getProducts.test.ts
@@ -1,15 +1,47 @@
-import { ProductBusiness } from '../../../src/business/ProductBusiness'
-import { IdGeneratorMock } from '../../mocks/IdGeneratorMock'
-import { ProductDatabaseMock } from '../../mocks/ProductDatabaseMock'
-import { TokenManagerMock } from '../../mocks/TokenManagerMock'
+import { ProductBusiness } from "../../../src/business/ProductBusiness";
+import { GetProductsSchema } from "../../../src/dtos/product/getProducts.dto";
+import { BadRequestError } from "../../../src/errors/BadRequestError";
+import { IdGeneratorMock } from "../../mocks/IdGeneratorMock";
+import { ProductDatabaseMock } from "../../mocks/ProductDatabaseMock";
+import { TokenManagerMock } from "../../mocks/TokenManagerMock";
 
 describe("Testando getProducts", () => {
   const productBusiness = new ProductBusiness(
     new ProductDatabaseMock(),
     new IdGeneratorMock(),
     new TokenManagerMock()
-  )
+  );
 
-  test("faça seus testes", async () => {
-  })
-})
+  // Teste 1 retornar produtos 
+  test("deve retornar token", async () => {
+    const input = GetProductsSchema.parse({
+      token: "token-mock-astrodev",
+    });
+    const output = await productBusiness.getProducts(input);
+    // console.log(output);
+    expect(output).toHaveLength(2);
+    expect(output).toContainEqual({
+      id: "p001",
+      name: "Mouse",
+      price: 50,
+      createdAt: expect.any(String),
+    });
+  });
+  // teste 2 Token invalido getProducts
+  test("deve retornar erro token invalido", async () => {
+    expect.assertions(1)
+    try {
+      const input = GetProductsSchema.parse({
+        token: "token-mock-astrodev2"
+      })
+      await productBusiness.getProducts(input)
+      
+    } catch (error) {
+      if (error instanceof BadRequestError ) {            
+        expect(error.message).toBe("token inválido")
+      }
+    }
+  });
+
+  
+});
--- tests/business/user/login.test.ts
+++ tests/business/user/login.test.ts
@@ -1,9 +1,11 @@
-import { UserBusiness } from "../../../src/business/UserBusiness"
-import { LoginSchema } from "../../../src/dtos/user/login.dto"
-import { HashManagerMock } from "../../mocks/HashManagerMock"
-import { IdGeneratorMock } from "../../mocks/IdGeneratorMock"
-import { TokenManagerMock } from "../../mocks/TokenManagerMock"
-import { UserDatabaseMock } from "../../mocks/UserDatabaseMock"
+import { UserBusiness } from "../../../src/business/UserBusiness";
+import { LoginSchema } from "../../../src/dtos/user/login.dto";
+import { BadRequestError } from "../../../src/errors/BadRequestError";
+import { NotFoundError } from "../../../src/errors/NotFoundError";
+import { HashManagerMock } from "../../mocks/HashManagerMock";
+import { IdGeneratorMock } from "../../mocks/IdGeneratorMock";
+import { TokenManagerMock } from "../../mocks/TokenManagerMock";
+import { UserDatabaseMock } from "../../mocks/UserDatabaseMock";
 
 describe("Testando login", () => {
   const userBusiness = new UserBusiness(
@@ -11,19 +13,51 @@ describe("Testando login", () => {
     new IdGeneratorMock(),
     new TokenManagerMock(),
     new HashManagerMock()
-  )
+  );
 
   test("deve gerar token ao logar", async () => {
     const input = LoginSchema.parse({
       email: "fulano@email.com",
-      password: "fulano123"
-    })
+      password: "fulano123",
+    });
 
-    const output = await userBusiness.login(input)
+    const output = await userBusiness.login(input);
 
     expect(output).toEqual({
       message: "Login realizado com sucesso",
-      token: "token-mock-fulano"
-    })
-  })
-})
+      token: "token-mock-fulano",
+    });
+  });
+  // teste erro de email nao encontrado
+  test("deve retornar erro email não encontrado", async () => {
+    expect.assertions(1);
+    try {
+      const input = LoginSchema.parse({
+        email: "fulanoDequem@email.com",
+        password: "fulano123",
+      });
+
+      await userBusiness.login(input);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        expect(error.message).toBe("'email' não encontrado");
+      }
+    }
+  });
+  // deve retornar erro de senha
+  test("deve retornar erro senha incorreta", async () => {
+    expect.assertions(1);
+    try {
+      const input = LoginSchema.parse({
+        email: "fulano@email.com",
+        password: "fulano1235",
+      });
+
+      await userBusiness.login(input);
+    } catch (error) {
+      if (error instanceof BadRequestError) {
+        expect(error.message).toBe("'email' ou 'password' incorretos");
+      }
+    }
+  });
+});
